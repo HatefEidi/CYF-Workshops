3b22aa738487b8722c3ecdc379fbad3012f7924e
Hatef Eidi <hatef.121.h@gmail.com>
2025-02-15 23:53:54 +0000

    Let variable when changing the value

--- js1-week-2/6.js
+++ js1-week-2/6.js
@@ -5,7 +5,7 @@
 function printTimesTables(n) {
     let count = 1;
     while(count <= 12) {
-        const product = n * count;
+        let product = n * count;
         console.log(`${n} * ${count} = ${product}`);
         count++;
     }
